c2fc49f8341f15ccd9906b74de19a994e4341122
sachin624k <sachin77000k@gmail.com>
2026-01-11 19:53:45 +0530

    Initial commit: made the weather search actually work

---
+++ mini-project-react/src/SearchBox.jsx
@@ -0,0 +1,55 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./SearchBox.css";
+import { useState } from 'react';
+
+export default function SearchBox() {
+    let [city, setCity] = useState("");
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "64606085d8522fb54a5ef82393578e99";
+
+    let getWeatherInfo = async () => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        let result = {
+            temp: jsonResponse.main.temp,
+            tempMin: jsonResponse.main.temp_min,
+            tempMax: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feelsLike: jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description,
+        };
+        console.log(result);
+    };
+
+    let handleChange = (evt) => {
+        setCity(evt.target.value);
+    };
+
+    let handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(city);
+        setCity("");
+        getWeatherInfo();
+    };
+
+    return (
+        <div className="SearchBox">
+            <h3>Search for the weather</h3>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    id="city"
+                    label="City Name"
+                    variant="outlined"
+                    required value={city}
+                    onChange={handleChange}
+                />
+                <br></br><br></br>
+                <Button variant="contained" type="submit">
+                    Search
+                </Button>
+            </form>
+        </div>
+    );
+}
